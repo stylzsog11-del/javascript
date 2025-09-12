2e1cdd65a2d0f67badff4eb71f7e2982f4d11351
Myron Richardson <e1436803@student.dcccd.edu>
2025-09-12 09:15:20 -0500

    script update

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    var visitorName = prompt("What is your name?");
-    if (visitorName) {
-        alert("Welcome to my site, " + visitorName + "!");
-        document.querySelector("h1").textContent += " - Hello, " + visitorName + "!";
-    }
-};
