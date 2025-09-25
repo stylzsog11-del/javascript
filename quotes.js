349994d241614bb54e19dc634b8d0163ca61b33c
Myron Richardson <e1436803@student.dcccd.edu>
2025-09-25 14:31:24 -0500

    javascript quotes updates

--- quotes.js
+++ quotes.js
@@ -21,7 +21,7 @@ let websites = [
     "https://www.w3schools.com/",
     "https://developer.mozilla.org/"
 ];
-displayWebsites(websites);
+document.getElementById("websites").textContent = websites;
 // Function to display websites as links
 function displayWebsites(arr) {
     const ul = document.getElementById("websites");
